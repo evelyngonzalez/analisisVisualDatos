704c06e28ee9053d5bf122a4101bf67423cf06b5
evelyngonzalez <evevane326@gmail.com>
2018-11-12 20:49:08 -0300

    add graphs

--- web/server.js
+++ web/server.js
@@ -13,6 +13,18 @@ app.get('/', function(req, res) {
   res.render('index');
 });
 
+app.get('/index', function(req, res) {
+  res.render('index');
+});
+
+app.get('/estadisticas', function(req, res) {
+  res.render('estadisticas');
+});
+
+app.get('/carreraGraph', function(req, res) {
+  res.render('carreraGraph');
+});
+
 app.listen(8000, function() {
   console.log('Listening!');
 });
